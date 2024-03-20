1d0713c6f031df1e190bb5f4dbe2669ec525559b
Erkz <erkiparna7@gmail.com>
2024-03-20 10:40:12 +0200

    File duplication fix

--- src/config/config.ts
+++
@@ -1,9 +0,0 @@
-
-const config = {
-    ip: 'http://localhost:3000',
-    production: false,
-    user_accounts: true,
-    email: ''
-}
-
-export default config;
--- src/config/router_handler.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import userRouter from '../routes/users';
-
-const configureRouters = (app : Router) => {
-    app.use(userRouter);
-};
-
-export default configureRouters;
